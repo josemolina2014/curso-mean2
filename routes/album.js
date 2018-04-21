bade944870f91d86104c83f566f6cb27e1a78e0d
jose molina <josmoli@gmail.com>
2018-04-21 14:33:04 -0500

    Clase 38 - Métodos para las imágenes de albums

--- routes/album.js
+++ routes/album.js
@@ -7,21 +7,15 @@ var md_auth = require('../middelwares/authenticate');
 
 var multipart = require('connect-multiparty');
 
-var md_upload = multipart({ uploadDir : './uploads/album'});
+var md_upload = multipart({ uploadDir : './uploads/albums'});
 
 api.get('/album/:id', md_auth.ensureAuth, AlbumController.getAlbum);
 api.post('/album', md_auth.ensureAuth, AlbumController.saveAlbum);
 api.get('/albums/:artist?', md_auth.ensureAuth, AlbumController.getAlbums);
 api.put('/album/:id', md_auth.ensureAuth, AlbumController.updateAlbum);
 api.delete('/album/:id',md_auth.ensureAuth, AlbumController.deleteAlbum);
-
-/*
-
-
-
-api.post('/upload-image-artist/:id',[md_auth.ensureAuth, md_upload], ArtistController.uploadImage);
-api.get('/get-image-artist/:imageFile',ArtistController.getImageFile);
-*/
+api.post('/upload-image-album/:id',[md_auth.ensureAuth, md_upload], AlbumController.uploadImage);
+api.get('/get-image-album/:imageFile',AlbumController.getImageFile);
 
 module.exports = api;
 
