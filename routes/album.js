5b98dd901d12ca8466560e1c03815a19b1fd2444
jose molina <josmoli@gmail.com>
2018-04-21 13:28:07 -0500

    Clase 33 - Guardar un album

--- routes/album.js
+++ routes/album.js
@@ -10,8 +10,8 @@ var multipart = require('connect-multiparty');
 var md_upload = multipart({ uploadDir : './uploads/album'});
 
 api.get('/album/', md_auth.ensureAuth, ALbumController.getAlbum);
-
-/*api.post('/artist', md_auth.ensureAuth, ArtistController.saveArtist);
+api.post('/album', md_auth.ensureAuth, ALbumController.saveAlbum);
+/*
 api.get('/artists/:page?', md_auth.ensureAuth, ArtistController.getArtists);
 api.put('/artist/:id', md_auth.ensureAuth, ArtistController.updateArtist);
 api.delete('/artist/:id',md_auth.ensureAuth, ArtistController.deleteArtist);
