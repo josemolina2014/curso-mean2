9ef3d5fe3120b788ca437ea563162d257b5e2d37
jose molina <josmoli@gmail.com>
2018-04-21 12:22:28 -0500

    Clase 31 - Métodos las imágenes del artista

--- controllers/artist.js
+++ controllers/artist.js
@@ -160,10 +160,67 @@ function deleteArtist(req, res)
 
 }
 
+function uploadImage(req, res) 
+{
+	var artistId = req.params.id;
+	var file_name = 'No subido...';
+
+	if (req.files) 
+		{
+			var file_path = req.files.image.path;
+			var file_split = file_path.split('\\');
+			var file_name = file_split[2];
+			var ext_split = file_name.split('\.');
+
+			var file_ext = ext_split[1];
+			if (file_ext=='png' || file_ext == 'jpg' || file_ext =='gif') 
+				{
+					Artist.findByIdAndUpdate(artistId, {image : file_name}, (err, artistUpdated) => 
+					{
+						if (err) 
+						{
+							res.status(500).send({message: 'Error al actualizar el usuario'});		
+						} 
+						else {
+							if (!artistUpdated) {
+								res.status(404).send({message: 'No se ha podido actualizar el usuario'});
+							}
+							else{
+								res.status(200).send({artist : artistUpdated});	
+							}
+						}
+					});
+				} 
+				else 
+				{
+					res.status(200).send({message : 'Extensión del archivo invalida'});
+				}
+			console.log(file_path);
+		} else 
+		{
+			res.status(200).send({message : 'No has subidoninguna imagen'});
+		}
+}
+
+function getImageFile(req, res) 
+{
+	var imageFile = req.params.imageFile;
+	var path_file = './uploads/artists/'+imageFile;
+	fs.exists(path_file, function function_name(exists) {
+		if (exists) {
+			res.sendFile(path.resolve(path_file));
+		} else {
+			res.status(200).send({message: 'No existe la imagen'});
+		}
+	});
+}
+
 module.exports ={
 	getArtist,
 	saveArtist,
 	getArtists,
 	updateArtist,
-	deleteArtist
+	deleteArtist,
+	uploadImage,
+	getImageFile
 }
