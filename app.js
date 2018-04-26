02a5f917c56f7361216afd0a6157a7d9abd55e48
jose molina <josmoli@gmail.com>
2018-04-25 21:01:48 -0500

    Clase 46 - Configurar cabeceras (CORS)

--- app.js
+++ app.js
@@ -15,6 +15,15 @@ var song_routes= require('./routes/song');
  app.use(bodyParser.json());
 
  //configurar cabecertas http
+app.use( (req, res, next) => {
+
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Autorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept,Access-Allow-Request-Method');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+	res.header('Allow','GET, POST, OPTIONS, PUT, DELETE');
+	next();
+});
+
 
  // rutas base
 app.use('/api', user_routes);
