0e47adaec0eb594ace975958c98b00ebe500f630
jose molina <josmoli@gmail.com>
2018-05-12 15:57:45 -0500

    Clase 76 - Marcar elementos del menú con Angular 2

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -9,6 +9,11 @@ import {UserEditComponent} from './components/user-edit.component';
 import { ArtistListComponent} from './components/artist-list.component';
 
 const appRoutes: Routes = [
+	{
+		path: '',
+		redirectTo: '/artists/1',
+		pathMatch: 'full'
+	},
 	
 	{path: '', component: ArtistListComponent },
 	{path: 'artists/:page', component: ArtistListComponent },
