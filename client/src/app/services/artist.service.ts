b31c2abae3896c804e63c790cecd93f52bce995a
jose molina <josmoli@gmail.com>
2018-05-17 21:22:53 -0500

    Clase 84 - Métodos en el servicio de artista

--- client/src/app/services/artist.service.ts
+++ client/src/app/services/artist.service.ts
@@ -1,16 +1,39 @@
 import { Injectable } from '@angular/core';
-import {Http, Response, Headers} from '@angular/http';
+import {Http, Response, Headers, RequestOptions} from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
 import { GLOBAL} from './global';
 import {Artist} from '../models/artist';
 
 @Injectable()
-export class ArtistService {
+export class ArtistService 
+{
 	public url: string;
 	
 	constructor(public _http: Http){
 		this.url = GLOBAL.url;
+	}
+
+	getArtists(token, page){
+		let headers = new Headers({
+			'Content-Type': 'application/json',
+			'Authorization' : token
+		});
+
+		let options = new RequestOptions({headers: headers});
+		return this._http.get(this.url+'artists/'+page, options)
+						.map(res => res.json());
+	}
+
+	getArtist(token, id:string){
+		let headers = new Headers({
+			'Content-Type': 'application/json',
+			'Authorization' : token
+		});
+
+		let options = new RequestOptions({headers: headers});
+		return this._http.get(this.url+'artist/'+id, options)
+						.map(res => res.json());
 	}
 
 	addArtist(token, artist: Artist){
@@ -23,5 +46,28 @@ export class ArtistService {
 
 		return this._http.post(this.url+'artist', params, {headers:  headers})
 					.map(res => res.json());
+	}
+
+	editArtist(token, id:string, artist: Artist){
+
+		let params = JSON.stringify(artist);
+		let headers = new Headers({
+			'Content-Type': 'application/json',
+			'Authorization' : token
+		});
+
+		return this._http.put(this.url+'artist/'+id, params, {headers:  headers})
+					.map(res => res.json());
+	}
+
+	deleteArtist(token, id:string){
+		let headers = new Headers({
+			'Content-Type': 'application/json',
+			'Authorization' : token
+		});
+
+		let options = new RequestOptions({headers: headers});
+		return this._http.delete(this.url+'artist/'+id, options)
+						.map(res => res.json());
 	}
 }
