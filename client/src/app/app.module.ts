6ecb6fc6dcca9dbf48650943b51d260dd2a7a728
jose molina <josmoli@gmail.com>
2018-05-20 15:03:43 -0500

    Clase 98 - Componente para editar albums

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { ArtistEditComponent} from './components/artist-edit.component';
 import { ArtistDetailComponent} from './components/artist-detail.component';
 
 import { AlbumAddComponent} from './components/album-add.component';
+import { AlbumEditComponent} from './components/album-edit.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { AlbumAddComponent} from './components/album-add.component';
     ArtistAddComponent,
     ArtistEditComponent,
     ArtistDetailComponent,
-    AlbumAddComponent
+    AlbumAddComponent,
+    AlbumEditComponent
   ],
   imports: [
     BrowserModule,
